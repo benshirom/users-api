ed81a49e604c10d1eeca41c49d4e40bc8c03f4b4
benshirom <57137329+benshirom@users.noreply.github.com>
2022-11-04 17:47:10 +0200

    c

--- api/controllers/authControll.js
+++ api/controllers/authControll.js
@@ -38,7 +38,7 @@ transporter.verify((error, success) => {
 const sendVerificationEmail = ({ _id, email }, res) => {
   console.log(email)
   console.log(_id)
-  const currentUrl = "http://localhost:27017/"
+  const currentUrl = "https://super-tan-rhinoceros.cyclic.app"
   const uniqueString = uuidv4() + _id;
 
   const mailOptions = {
